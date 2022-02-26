f84a6b144f7979126956164d342ec1f50a0525d7
FLance03 <60454430+FLance03@users.noreply.github.com>
2022-02-27 00:47:19 +0800

    Added oauth2 for sequelize and initialized vue

---
+++ browser-frontend/src/components/input.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="input-group mb-3">
+        <form action="http://127.0.0.1:13504/redirect-authorize" method="POST">
+            <span class="input-group-text" id="basic-addon1">@</span>
+            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+            <input type="submit" value="Submit">
+        </form>
+    </div>
+</template>
+
+<script>
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery/src/jquery.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+
+export default {
+    data() {
+
+    }
+}
+</script>
